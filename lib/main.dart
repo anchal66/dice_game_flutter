72a21d5ca02fa4b3ba32e7dff13b72d3227a9515
anchal66 <anchalavinash66@gmail.com>
2021-10-18 16:00:13 +0530

    Added dice images and impl for change on Click of image

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int randomNumber = 1;
+
+  generateNumber() {
+    setState(() {
+      randomNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("Dice"),
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: TextButton(
+                  style:
+                      const ButtonStyle(splashFactory: NoSplash.splashFactory),
+                  onPressed: () {
+                    generateNumber();
+                  },
+                  child: Image.asset("images/dice$randomNumber.png"),
+                ),
+              ),
+            ),
+            Expanded(
+                child: Text(
+              "You scored is: $randomNumber",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ))
+          ],
+        ),
+      ),
+    ));
   }
 }
